f15f8e8a14801b555011666eb565cd6adc43b0fb
Marcio Silva <marcionps@proton.me>
2023-07-02 13:19:11 -0300

    refactor - create a function to lower case and sort word

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 const isAnagram = (word1, word2) => {
-  sorted_word1 = word1.toLowerCase().split('').sort().join('')
-  sorted_word2 = word2.toLowerCase().split('').sort().join('')
-
-  if (sorted_word1 === sorted_word2) {
-    return true;
-  }
-
-  return false;
+  return lowerCaseAndSort(word1) === lowerCaseAndSort(word2)
 };
 
+const lowerCaseAndSort = (word) => {
+  return word.toLowerCase().split('').sort().join('')
+}
+
 module.exports = {
+  lowerCaseAndSort,
   isAnagram,
 };
